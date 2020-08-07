31ab29ade4b290bf41d9c4afa92ed2c04523cf2f
sheepsbleat <adamypadan@gmail.com>
2020-08-07 11:07:06 +0100

    break test

--- src/App.test.js
+++ src/App.test.js
@@ -3,12 +3,6 @@ import ReactDOM from "react-dom";
 import App from "./App";
 import { BrowserRouter } from "react-router-dom";
 
-it("renders without crashing", () => {
-  const div = document.createElement("div");
-  ReactDOM.render(
-    <BrowserRouter>
-      <App />
-    </BrowserRouter>,
-    div
-  );
+it("works", () => {
+  expect(5).toBe(2);
 });
