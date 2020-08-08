385683742a6e9bcf8bc5c6d0489f352b2c0f6064
sheepsbleat <adamypadan@gmail.com>
2020-08-08 14:11:43 +0100

    🚑 Critical Hotfix:  lowered margin

--- src/DraggableColorList.js
+++ src/DraggableColorList.js
@@ -4,7 +4,7 @@ import { SortableContainer } from "react-sortable-hoc";
 
 const DraggableColorList = SortableContainer(({ colors, removeColor }) => {
   return (
-    <div style={{ height: "96%", marginTop: "4%" }}>
+    <div style={{ height: "100%" }}>
       {colors.map((color, i) => (
         <DraggableColorBox
           index={i}
